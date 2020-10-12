4c49990fa812f86b778fda29075dba20c2a71d90
julian layen <julian@codecodex.fr>
2020-10-12 14:49:20 +0200

    debug

--- user/create.php
+++ user/create.php
@@ -21,7 +21,7 @@ $db = $database->getConnection();
 $user = new User($db);
 
 // get posted data
-$data = json_decode(file_get_contents("php://input"));
+$data = json_decode(file_get_contents("php://input"), true);
 
 // make sure data is not empty
 if(
