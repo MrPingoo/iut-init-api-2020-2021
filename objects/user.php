478906b11777a14cbe5afe8ae3e5dbf226cb65f6
julian layen <julian@codecodex.fr>
2020-10-20 15:04:43 +0200

    User paginator

--- objects/user.php
+++ objects/user.php
@@ -72,10 +72,6 @@ class User{
         $data = json_encode($this->roles);
         $stmt->bindParam(":roles", $data);
 
-        $stmt->execute();
-        var_dump($stmt->errorInfo());
-        die();
-
         // execute query
         if($stmt->execute()){
             return $this->conn->lastInsertId();
@@ -96,6 +92,24 @@ class User{
                 " . $this->table_name . " u
             WHERE 
                 u.id = $id";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // read one user
+    function list($limit = 10, $offset = 0){
+        // select all query
+        $query = "SELECT
+                *
+            FROM
+                " . $this->table_name . " u
+            LIMIT $limit OFFSET $offset";
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
